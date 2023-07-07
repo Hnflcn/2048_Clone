64f9653b640dde7f6a7d00fb1298b84d053d7ffb
Hnflcn <58750718+Hnflcn@users.noreply.github.com>
2023-07-08 00:35:28 +0300

    moving bug çözüldü

--- 2048_Clone/Assets/Scripts/Managers/TileManager.cs
+++ 2048_Clone/Assets/Scripts/Managers/TileManager.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Base;
 using DG.Tweening;
+using Lean.Touch;
 using Tile;
 using UnityEngine;
 using UnityEngine.UI;
@@ -87,7 +88,7 @@ namespace Managers
             return randomValue <= .8f ? 2 : 4;
         }
 
-        private void UpdateTilePosition()
+        protected void UpdateTilePosition()
         {
             for (var i = 0; i < GridSize; i++)
             {
@@ -98,7 +99,233 @@ namespace Managers
                 }
             }
         }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        #region Variables
+
+        private Vector2 _startPosition;
+        private const float SwipeThreshold = 50f;
+
+        #endregion
+
+        #region LeanTouch Functions
+
+        private void OnEnable()
+        {
+            LeanTouch.OnFingerDown += OnFingerDown;
+            LeanTouch.OnFingerUp += OnFingerUp;
+        }
+
+        private void OnDisable()
+        {
+            LeanTouch.OnFingerDown -= OnFingerDown;
+            LeanTouch.OnFingerUp -= OnFingerUp;
+        }
+
+        private void OnFingerDown(LeanFinger finger)
+        {
+            _startPosition = finger.ScreenPosition;
+        }
+
+        private void OnFingerUp(LeanFinger finger)
+        {
+            var currentPosition = finger.ScreenPosition;
+            var direction = currentPosition - _startPosition;
+
+            if (!(direction.magnitude >= SwipeThreshold)) return;
+            direction.Normalize();
+
+            DefineFingerMoving(direction);
+        }
+
+        #endregion
+
+
+        #region MoveDirection
+
+        private void DefineFingerMoving(Vector2 direction)
+        {
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                if (direction.x > 0)
+                    MoveRight();
+                else
+                    MoveLeft();
+            }
+            else
+            {
+                if (direction.y > 0)
+                    MoveUp();
+                else
+                    MoveDown();
+            }
+            UpdateTilePosition();
+        }
+        
+        
+          private void MoveRight()
+          {
+              Debug.Log("sag");
+              for (var j = 0; j < GridSize; j++)
+                  for (var i= GridSize - 1; i >= 0; i--)
+                  {
+                      Debug.Log("sag1");
+                      if (Tiles[i,j] == null) continue; 
+                      
+                      for (var k = GridSize - 1; k > i; k--)
+                      {
+                          Debug.Log("sag2");
+                          if (Tiles[k,j] != null) continue;
+                                  Tiles[k, j] = Tiles[i, j];
+                                  
+                          
+                          Debug.Log("sag3");
+                          Tiles[i, j] = null;
+                          break;
+                      }
+                  }
+          }
+          private void MoveLeft()
+          {
+              Debug.Log("sol");
+              for (var j = 0; j < GridSize; j++)
+                  for (var i=0; i<GridSize; i++)
+                  {
+                      if (Tiles[i,j] == null) continue;
+                              for (var k = 0; k < i; k++)
+                      {
+                          if (Tiles[k,j] != null) continue;
+                                  Tiles[k, j] = Tiles[i, j];
+                          Tiles[i, j] = null;
+                          break;
+                      }
+                  }
+          }
+        private void MoveDown()
+          {
+              Debug.Log("aşağı");
+              for (var i = 0; i < GridSize; i++)
+                  for (var j = GridSize-1; j>=0; j--)
+                  {
+                      if(Tiles[i,j] == null) continue;
+                      for (var k = GridSize-1; k >j; k--)
+                      {
+                          if(Tiles[i,k] != null) continue;
+                                  Tiles[i, k] = Tiles[i, j];
+                          Tiles[i, j] = null;
+                          break;
+                      }
+                  }
+              
+          }
+        private void MoveUp()
+          {
+              Debug.Log("yukarı");
+              for (var i = 0; i < GridSize; i++)
+                  for (var j =0; j<GridSize; j++)
+                  {
+                      if(Tiles[i,j] == null) continue;
+                      for (var k = 0; k < j; k++)
+                      {
+                          if(Tiles[i,k] != null) continue;
+                                  Tiles[i, k] = Tiles[i, j];
+                          Tiles[i, j] = null;
+                          break;    
+                      }
+                  }
+          }
+
+        #endregion
     }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
 
